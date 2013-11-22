82440bc80dacb5b23cbcf476292db2fc526fa54d
luisbiato <luisf.biato@gmail.com>
2013-11-22 09:20:11 -0200

    Dados Bloomber Comentado

--- Dados Bloomberg.R
+++ Dados Bloomberg.R
@@ -1,19 +1,20 @@
 require(xlsx)
 require(matlab)
 
-
+data = read.csv("/Users/Luis/Documents/GitHub/Computational-Investment/Tabelas/Datas.csv") # Pegar Datas
+data = matrix(data[,1])
 caminho = paste("/Users/Luis/Documents/GitHub/Computational-Investment/Tabelas/Parte", 1:82, ".xlsx", sep = "")
 Ativos = c()
 n = length(caminho)-1
 
 for (k in 1:n)
 {
-  for (i in 1:20)
+  for (i in 1:20)     # Ler arquivos XLSX
   {
     Temp = read.xlsx(caminho[k], i, header=FALSE)
     Temp1 = matrix(Temp[1,1])
     Temp = matrix(Temp[,2])
-    if (size(Temp,1) < 8434)
+    if (nrow(Temp) < 8434)       # Para pegar o dado mesmo que esteja com problemas
     {
       Temp = matrix(,8434,)
       Temp[1] = Temp1
@@ -21,7 +22,7 @@ for (k in 1:n)
       print(i + (k-1)*20)          # Pra ver o progresso
       
     } else {
-      Temp = matrix(Temp[2:size(Temp,1),])
+      Temp = matrix(Temp[2:nrow(Temp),])
       Temp[1] = Temp1
       Ativos = cbind(Ativos,Temp)
       print(i + (k-1)*20)          # Pra ver o progresso
@@ -34,31 +35,31 @@ for (j in 1:14)   # Ultima parte tem somente 14 planilhas
   Temp = read.xlsx(caminho[82],j,header=FALSE)
   Temp1 = matrix(Temp[1,1])
   Temp = matrix(Temp[,2])
-  Temp = matrix(Temp[2:size(Temp,1),])
+  Temp = matrix(Temp[2:nrow(Temp),])
   Temp[1] = Temp1
   Ativos = cbind(Ativos,Temp)
   print(j)          # Pra ver o progresso
 }
- 
-nomes = Ativos[1,]
+
+nomes = Ativos[1,]          # Colcar label
 Ativos = Ativos[-1,]
 
 
 ativos = c()
-for (i in 1:size(Ativos,2))
+for (i in 1:ncol(Ativos))            # Transformar a matriz em numerica
 {
   temp = as.numeric(Ativos[,i])
   ativos = cbind(ativos,temp)
   print(i)
 }
 
-Ativos = ativos
-colnames(Ativos) = nomes
+Ativos = ativos      # Transformar Ativos em numerica
+colnames(Ativos) = nomes          # Colcar label
 
 
-for (i in 1:size(Ativos,2))
+for (i in 1:ncol(Ativos))        # Trocar "NA" por 0
 {
-  for (k in 1:size(Ativos,1))
+  for (k in 1:nrow(Ativos))
   {
     if(nchar(Ativos[k,i]) == 2)
     {
@@ -72,10 +73,10 @@ soma = 0
 Temp = c()
 nome = c()
 errados = c()
-for (i in 1:size(Ativos,2))
+for (i in 1:ncol(Ativos))         # Excluir coluna se sua soma for 0 (se nao houver dados)
 {
   soma = 0
-  for (k in 1:size(Ativos,1))
+  for (k in 1:nrow(Ativos))
   {
     temp = Ativos[k,i]
     soma = soma + temp
@@ -84,20 +85,20 @@ for (i in 1:size(Ativos,2))
   if (soma > 0)
   {
     Temp = cbind(Temp,ativos[,i])
-    nome = c(nome, nomes[i])
+    nome = c(nome, nomes[i])          # Colocar label
   } else 
   {
     Temp1 = i
-    errados = c(errados, Temp1)
+    errados = c(errados, Temp1)        # Ver quais ativos deram erro
   }
   print(i)
 }
 
 Ativos = Temp
-colnames(Ativos) = nome
+colnames(Ativos) = nome               # Colocar label
 
 Nomes = c()
-for (i in 1:length(errados))
+for (i in 1:length(errados))         # Nome dos ativos que deram erro
 {
   Nomes = c(Nomes,nomes[errados[i]])
 }
@@ -105,5 +106,25 @@ Nomes = t(Nomes);Nomes = t(Nomes)
 
 
 
-# write.csv(Ativos, "C:/Users/b2476514/Desktop/Dados.csv", row.names = FALSE)
+ativos = Ativos
+Ativos = cbind(data, Ativos)         # Colocar data na matrix Ativos
 
+for (i in 1:ncol(Ativos))        # Trocar "NA" por ""
+{
+  for (k in 1:nrow(Ativos))
+  {
+    if(nchar(Ativos[k,i]) == 2)
+    {
+      Ativos[k,i] = ""
+    } 
+  }
+  print(i)
+}
+
+
+
+
+
+
+write.csv(Ativos, "/Users/Luis/Documents/GitHub/Computational-Investment/Dados Bloomberg.csv", row.names = FALSE) # Para visualização
+write.csv(ativos, "/Users/Luis/Documents/GitHub/Computational-Investment/Dados.csv", row.names = FALSE)    # Para calculo 
